8677774342f537d2abbceed2b574738a6bc1e4ef
johnphilipsprague <jpsprague95@gmail.com>
2022-04-19 15:25:56 -0500

    cart CRUD routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,16 +2,49 @@ const router = require('express').Router()
 // const { models: { User }} = require('../db') <- get export format from db
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+// GET /api/cart/:id
+// Access: User only
+router.get('/:id', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username'],
-    })
-    res.json(users)
+    const usersCart = await Cart.findByPk(req.params.id)
+    res.send(usersCart)
   } catch (err) {
     next(err)
   }
 })
+
+// POST /api/cart/
+// Access: system process
+// router.post('/', async (req, res, next) => {
+//   try {
+//     res.status(201).send(await Cart.create(req.body))
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+// PUT /api/cart/:id
+// Access: User only
+router.put('/:id', async (req, res, next) => {
+  try {
+    const cartToEdit = await Cart.findByPk(req.params.id)
+    await cartToEdit.update(req.body)
+    res.send(cartToEdit)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// DELETE /api/cart/:id
+// Access: system process
+// router.delete('/:id', async (req, res, next) => {
+//   try {
+//     const cartToDelete = await Cart.findByPk(req.params.id)
+//     await cartToDelete.destroy()
+//     res.send(cartToDelete)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+//

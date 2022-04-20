37838d03be82d5ac586c683102ecf53b2a7f9d41
johnphilipsprague <jpsprague95@gmail.com>
2022-04-20 16:45:03 -0500

    refactored api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router
 // USE api/
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
-router.use('/cart', require('./cart'))
+router.use('/order', require('./order'))
 
 // general 404 error handler
 router.use((req, res, next) => {

6e1d14380c031c5ef0fcdf73356959f7668d681a
Justin Duplain <82243533+pmentropy@users.noreply.github.com>
2022-04-19 21:40:50 -0400

    cart holds quantity based on associations
    Instead of using a JSON 'items' object I think we can use the associations to do the work for us, once that is in place the only thing to keep track of is quantity.

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-  items: {
-    type: Sequelize.JSON,
+  qty: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
   },
 })
 

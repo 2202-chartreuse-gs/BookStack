92c4dd9f805ba09dae53ee708506aa4d1a814197
MichaelMnatsakanian <michaelmnat@gmail.com>
2022-04-21 12:10:12 -0800

    Rendering single product on website

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import { withRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm'
 import Home from './components/Home'
 import { me } from './store'
+import SingleProduct from './components/SingleProduct'
 
 /**
  * COMPONENT
@@ -28,6 +29,7 @@ class Routes extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path="/products/:productId" component={SingleProduct} />
           </Switch>
         )}
       </div>

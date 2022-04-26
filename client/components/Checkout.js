91a3424daaf975155f58b932605eb419a538a7f8
johnphilipsprague <jpsprague95@gmail.com>
2022-04-26 13:31:19 -0500

    adding checkout component

---
+++ client/components/Checkout.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CameraIcon from '@material-ui/icons/PhotoCamera'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Link from '@material-ui/core/Link'
+import AllProducts from './AllProducts'
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}))
+/**
+ * COMPONENT
+ */
+const Checkout = () => {
+  // Material UI Styles hook
+  const classes = useStyles()
+
+  return (
+    <div>
+      {/* Hero unit */}
+      <div className={classes.heroContent}>
+        <Container maxWidth="sm">
+          {/* <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            {firstName ? firstName : 'Guest'}
+          </Typography> */}
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Thanks for shopping here!
+          </Typography>
+          <div className={classes.heroButtons}>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item>
+                <Link href="/products/">
+                  <Button variant="contained" color="primary">
+                    View ALl Products
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout

5616ac9b32f148c96e6113beaf72f5195b10f20c
Teman Beck <temanbeck@yahoo.com>
2022-04-27 10:16:34 -0400

    hotfix: Cant resolve listitem module

--- client/components/AdminDashboard/index.js
+++ client/components/AdminDashboard/index.js
@@ -18,7 +18,7 @@ import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems, secondaryListItems } from './listitems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
